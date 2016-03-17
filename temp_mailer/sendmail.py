55382ee7a9691c9f2d1e73bf21115c3c12bae919
Siyaram <ersiyaram@gmail.com>
2016-03-17 11:46:16 +0530

    Modified

--- temp_mailer/sendmail.py
+++ temp_mailer/sendmail.py
@@ -7,20 +7,6 @@ class SendMail():
                 self.message = sendgrid.Mail()
         def send_mail(self,rcpt,frm,subject,msg):
 
-                """
-                        send_mail sends mail from GCE
-                        Arguments req :
-                                rcpt : a list of name and email ex :
-                                        ['Ravi <ravi.manik@innovaccer.com>','Ankit <ankit.maheshwari@innovaccer.com>']
-                                subject : "Subject of the mail"
-                                msg: "Body of the mail"
-                                frm: "from takes name a email" ex: 'Ravi <ravi.manik@innovaccer.com>''
-
-                                returns:
-                                        http_status_code
-                                                when exception is caught returns 400 or 500
-
-                """
 
                 self.message.add_to(rcpt)
                 self.message.set_subject(subject)
